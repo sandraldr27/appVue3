84adc94d636eaede9ff1bfb81ce10f0050541bd6
“sandraldr27” <sandralilder@gmail.com>
2023-02-26 21:20:34 +0100

    Advance third class of VUE's computed properties

--- src/components/myComponents/computed-properties/ComputerProps.vue
+++ src/components/myComponents/computed-properties/ComputerProps.vue
@@ -1,43 +1,55 @@
 <script>
-export default {
-    data() {
-        return {
-            comicTitle: 'Amazing Spiderman - Vol.1',
-            count:0,
-        }
-    },
-    computed: {
-        reversedComicTitle(){
-            return this.comicTitle.split('').reverse().join('')
-        },
-        barColor() {
+    export default {
+        data() {
             return {
-                'bg-sucess': this.count < 20,
-                'bg-warning': this.count >= 20 && this.count <=30,
-                'bg-danger': this.count > 30,
+                comicTitle: 'Amazing Spiderman - Vol.1',
+                count: 0,
+            }
+        },
+        computed:{
+            reversedComicTitle(){
+                return this.comicTitle.split('').reverse().join('')
+            },
+            barColor(){
+                return {
+                    'bg-sucess': this.count < 20,
+                    'bg-warning': this.count >= 20 && this.count <=30,
+                    'bg-danger': this.count > 30,
+                }
             }
         }
     }
-}
 </script>
 
 <template>
     <div class="my-4">
-        <h1>Computed Property</h1>
+        <h1>Computed property</h1>
         <div>
             <p>{{ comicTitle }}</p>
             <p>{{ reversedComicTitle }}</p>
-            <input type="text" v-model="comicTitle">
+            <input type="text" v-model="comicTitle" />
         </div>
         <div class="my-4">
             <button class="me-3 btn btn-primary" @click="count++">+</button>
-
+            <button class="btn btn-danger" @click="count--">-</button>
+            <h3>{{ count }}</h3>
+            <div class="progress">
+                <div
+                    :class="['progress-bar', barColor]"
+                    role="progressbar"
+                    :style="{width: count +'%'}"
+                    aria-valuenow="0"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    >
+                    {{ count }}%
+                </div>
+            </div>
         </div>
-
     </div>
-
 </template>
 
 <style lang="scss">
-    @import "bootstrap/dist/css/bootstrap.min.css";
+    @import "bootstrap/dist/css/bootstrap.min.css"
 </style>
+Footer
